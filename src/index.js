6346039db722490cb70c4c930fc943908edd23ba
Sreyasee97 <sreyaseepaharijgm@gmail.com>
2023-06-30 21:03:30 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import "./index.css"
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Clock(props) {
+  return (
+    <div className='box'>
+      <p className='paragraph'>Digital Clock</p>
+      <h1 className='heading'>{props.date.toLocaleTimeString()}</h1>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    </div>
+  )
+}
+
+function Timer() {
+  root.render(<Clock date={new Date()}/>)
+}
+setInterval(Timer,1000)
+
